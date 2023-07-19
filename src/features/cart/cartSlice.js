0c8b839107454e7a091d7cc28dde045c1e54e8d4
dani <danifromecuador@gmail.com>
2023-07-19 12:38:06 -0500

    Create CartContainer and CartItem components

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,8 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 import cartItems from "../../cartItems";
+
 const initialState = {
   cartItems: cartItems,
-  amount: 0,
+  amount: 4,
   total: 0,
   isLoading: true,
 };
