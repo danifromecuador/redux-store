4ba59eae659674793f44ad28a8098fe124869667
dani <danifromecuador@gmail.com>
2023-07-19 12:19:48 -0500

    Create Navbar component

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
+const initialState = {
+  cartItems: cartItems,
+  amount: 0,
+  total: 0,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+});
+
+export default cartSlice.reducer;
